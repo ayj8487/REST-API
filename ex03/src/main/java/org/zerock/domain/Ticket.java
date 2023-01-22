ca4ae771f83ed2c7a83c3d9966a247862d1bb5cc
ayj8487 <ayj8487@naver.com>
2023-01-22 09:40:39 +0900

    Rest방식 테스트 (Junit)

--- ex03/src/main/java/org/zerock/domain/Ticket.java
+++ ex03/src/main/java/org/zerock/domain/Ticket.java
@@ -9,7 +9,7 @@ public class Ticket {
 	// 또는 다양한 포맷의 입력 데이터를 반환 할 수 있다.
 	// 대부분 JSON 데이터를 서버에 보내서 원하는 탕비의 객체로 변환하는 용도로 사용
 	
-	private int bno; // 번호
+	private int tno; // 번호
 	private String owner;  //소유주 
 	private String grade; // 등급
 	
